7a72aad8f69e2eddb98d809dd64c371e0e1d16f6
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-06-08 07:55:53 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import Mylist from "./Lista.jsx";
+function TodoListApp() {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			< Mylist />
-			
-		</div>
-	);
-};
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' && task !== '') {
+      addTask();
+    }
+  };
 
-export default Home;
+  const addTask = () => {
+    setTaskList([...taskList, task]);
+    setTask('');
+  };
+
+  const deleteTask = (index) => {
+    const updatedTaskList = [...taskList];
+    updatedTaskList.splice(index, 1);
+    setTaskList(updatedTaskList);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(event) => setTask(event.target.value)}
+        onKeyPress={handleKeyPress}
+        placeholder="Agregar tarea"
+      />
+      <ul>
+        {taskList.length > 0 ? (
+          taskList.map((task, index) => (
+            <li key={index}>
+              {task}
+              <button onClick={() => deleteTask(index)}>Eliminar</button>
+            </li>
+          ))
+        ) : (
+          <li>No hay tareas, añadir tareas</li>
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoListApp;
+
